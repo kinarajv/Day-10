90169bae8717f124b7a9214f57339a2dde4814b4
LAPTOP-T4CR18B5\kinara <kinara.varga@outlook.com>
2023-03-31 13:46:39 +0700

    Day10

---
+++ Trial/Program.cs
@@ -0,0 +1,44 @@
+﻿
+class Program
+{
+	static void Main() 
+	{
+		PatientQueue pq = new PatientQueue();
+		pq.Enqueue(new Patient("John", 1));
+		pq.Enqueue(new Patient("Mary", 2));
+		pq.Enqueue(new Patient("Peter", 3));
+		List<Patient> pqe = pq.ToList();
+		
+		foreach(Patient p in pqe)
+		{
+			Console.WriteLine(p.Name + " " + p.Priority);
+		}
+	}
+}
+
+class PatientQueue
+{
+	private Queue<Patient> patients = new Queue<Patient>();
+	
+	public void Enqueue(Patient patient)
+	{
+		patients.Enqueue(patient);
+	}
+	
+	public List<Patient> ToList()
+	{
+		return patients.ToList();
+	}
+}
+
+class Patient 
+{
+	public string Name { get; set; }
+	public int Priority { get; set; }
+	
+	public Patient(string name, int priority)
+	{
+		Name = name;
+		Priority = priority;
+	}
+}
